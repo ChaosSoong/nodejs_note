d756dea81fc6ec9f5f65df81217f7fcaf1c9a30b
chaos <13313020563@163.com>
2016-11-04 14:01:20 +0800

    url.parse

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 var http = require("http");
+var url = require("url");
 var server = http.createServer(function(request, response){
-    response.writeHead(200,{'content-Type':'text/plain'});
-    response.end("Hello Chaos");
+    var pathname = url.parse(request.url).pathname;
+    if(pathname = "first"){
+        response.writeHead(200,{'Content-Type':'text/plain'});
+        response.end("Hello Chaos");
+    }
+    if(pathname = "second"){
+        console.log("Hello second");
+    }
+    
 });
 server.listen(8888);
 console.log("server startup at localhost:8888");
